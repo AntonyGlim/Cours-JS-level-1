c2e1304b86749dcd34e594c1d49faeeb6342d84b
AntonyGlim <antony.glim@gmail.com>
2021-07-24 13:58:19 +0300

    LS8 HW1

--- lesson_8/lesson_8_homework/app.js
+++ lesson_8/lesson_8_homework/app.js
@@ -28,3 +28,80 @@ let filterSizeWrap = document.querySelector('.filterSizeWrap');
 filterSizeWrap.addEventListener('click', function() {
     filterSizes.classList.toggle('hidden');
 });
+
+
+
+//change cart counter
+let cartCounter = document.querySelector("span.cartIconWrap span");
+let addToCartButtons = document.querySelectorAll("button.add-to-cart-button");
+for (const button of addToCartButtons) {
+    button.addEventListener("click", function(event) {
+        let counter = +cartCounter.innerText;
+        cartCounter.innerText = ++counter;
+        showOrHideCartCounter();
+    });
+}
+
+function showOrHideCartCounter() {
+    let counter = +cartCounter.innerText;
+    if (counter > 0) {
+        cartCounter.style.display = "inline-block";
+    } else {
+        cartCounter.style.display = "none";
+    }
+}
+
+showOrHideCartCounter();
+
+let cartTable = querySelector("table.cartTable");
+
+function findRowInTableByProductNameIfPresent(table, productName) {
+    for (const row in table.rows) {
+        if (row.cells[0] === productName) {
+            return row;
+        }
+    }
+    return null;
+}
+
+/**
+ * Get existent row or create new
+ * @param {*} cartTable - html table
+ * @param {*} product - DTO item
+ * @returns table row element
+ */
+function getOrCreateRow(cartTable, product) {
+    let row = findRowInTableByProductNameIfPresent(cartTable, product);
+    if (!row) {
+        row = cartTable.insertRow();
+    }
+    return row
+}
+
+/**
+ * Insetr new row in table or update existent
+ * @param {*} cartTable - html table
+ * @param {*} product - DTO item
+ */
+function addTableRow(cartTable, product) {
+    let row = getOrCreateRow(cartTable, product);
+
+    let cellProductName = row.insertCell(0);
+    let cellCount = row.insertCell(1);
+    let cellOneItemPrice = row.insertCell(2);
+    let cellTotalPrice = row.insertCell(3);
+
+    if (!cellProductName) { //add new row
+        cellProductName.innerHTML = product.name;
+        cellCount.innerHTML = 1;
+        cellOneItemPrice.innerHTML = product.price;
+    } else { //update existent
+        cellCount.innerHTML = +cellCount.innerHTML + 1;
+    }
+    cellTotalPrice.innerHTML = cellOneItemPrice.innerHTML * cellCount.innerHTML;
+}
+
+let cartIconWrap = document.querySelector("span.cartIconWrap");
+cartIconWrap.addEventListener("click", function(event) {
+    console.log("click");
+});
