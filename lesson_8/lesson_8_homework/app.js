e60400a58002aed2a9f9a5d0aec249952a2e9c66
AntonyGlim <antony.glim@gmail.com>
2021-08-02 17:51:12 +0300

    Remove table and revrite js with objects

--- lesson_8/lesson_8_homework/app.js
+++ lesson_8/lesson_8_homework/app.js
@@ -30,78 +30,121 @@ filterSizeWrap.addEventListener('click', function() {
 });
 
 
+//
+//
+//
 
-//change cart counter
 let cartCounter = document.querySelector("span.cartIconWrap span");
-let addToCartButtons = document.querySelectorAll("button.add-to-cart-button");
-for (const button of addToCartButtons) {
-    button.addEventListener("click", function(event) {
-        let counter = +cartCounter.innerText;
-        cartCounter.innerText = ++counter;
-        showOrHideCartCounter();
-    });
+
+/**
+ * Array with products and count:
+ * [ 
+ *      { 
+ *          name: "name",
+ *          price: "$50", 
+ *          text: "Description",
+ *          count: 4,
+ *          summ: "$200"
+ *      }
+ */
+let cart = [];
+
+/**
+ * Increment cart counter value
+ */
+function incrementCartCounterValue() {
+    let counter = +cartCounter.innerText;
+    cartCounter.innerText = ++counter;
+    showOrHideCartCounter();
 }
 
+/**
+ * Show or hide cart counter value
+ */
 function showOrHideCartCounter() {
     let counter = +cartCounter.innerText;
-    if (counter > 0) {
-        cartCounter.style.display = "inline-block";
-    } else {
-        cartCounter.style.display = "none";
+    if (counter > 0) cartCounter.style.display = "inline-block";
+    else cartCounter.style.display = "none";
+}
+
+/**
+ * Return name, price and text from html
+ * @param {MauseEvent} event 
+ * @returns product from html
+ */
+function buildProduct(event) {
+    let featuredItem = event.target.parentElement.parentElement.parentElement;
+    return {
+        name: featuredItem.querySelector("div.featuredName").innerText,
+        price: featuredItem.querySelector("div.featuredPrice").innerText,
+        text: featuredItem.querySelector("div.featuredText").innerText,
     }
 }
 
-showOrHideCartCounter();
+/**
+ * What we do on add-to-cart-button click
+ * @param {MauseEvent} event 
+ */
+function onAddToCartButtonClick(event) {
+    incrementCartCounterValue();
+    let product = buildProduct(event);
+    addProductToCart(product);
+}
 
-let cartTable = querySelector("table.cartTable");
+/**
+ * Add product to cart or update cart
+ * @param {*} product 
+ */
+function addProductToCart(product) {
+    let productFromCart = findProductInCartByProductNameOrNull(product.name);
+    if (productFromCart == null) { //insert new item
+        product.count = 1;
+        product.summ = product.price;
+        cart.push(product);
+    } else { //update existent item
+        productFromCart.count++;
+        productFromCart.summ = calculatePriceSumm(productFromCart.count, productFromCart.price);
+    }
+    console.log(cart);
+}
 
-function findRowInTableByProductNameIfPresent(table, productName) {
-    for (const row in table.rows) {
-        if (row.cells[0] === productName) {
-            return row;
+/**
+ * Find product in cart by product name or null
+ * @param {string} productName 
+ * @returns product with count
+ */
+function findProductInCartByProductNameOrNull(productName) {
+    for (let index = 0; index < cart.length; index++) {
+        if (cart[index].name === productName) {
+            return cart[index];
         }
     }
     return null;
 }
 
 /**
- * Get existent row or create new
- * @param {*} cartTable - html table
- * @param {*} product - DTO item
- * @returns table row element
+ * Preparing in advance productPrice (сut extra sumbols)
+ * Multiplay productsCount * productPrice 
+ * @param {number} productsCount - like 4
+ * @param {string} productPrice - start with $
  */
-function getOrCreateRow(cartTable, product) {
-    let row = findRowInTableByProductNameIfPresent(cartTable, product);
-    if (!row) {
-        row = cartTable.insertRow();
+function calculatePriceSumm(productsCount, productPrice) {
+    let price = productPrice;
+    if (price.startsWith("$")) {
+        price = price.substring(1, price.length - 1);
     }
-    return row
+    return "$" + productsCount * price;
 }
 
 /**
- * Insetr new row in table or update existent
- * @param {*} cartTable - html table
- * @param {*} product - DTO item
+ * Add event click listiner for all add-to-cart-button`s
  */
-function addTableRow(cartTable, product) {
-    let row = getOrCreateRow(cartTable, product);
-
-    let cellProductName = row.insertCell(0);
-    let cellCount = row.insertCell(1);
-    let cellOneItemPrice = row.insertCell(2);
-    let cellTotalPrice = row.insertCell(3);
-
-    if (!cellProductName) { //add new row
-        cellProductName.innerHTML = product.name;
-        cellCount.innerHTML = 1;
-        cellOneItemPrice.innerHTML = product.price;
-    } else { //update existent
-        cellCount.innerHTML = +cellCount.innerHTML + 1;
+function addEventClickListinerForAddToCartButtons() {
+    let addToCartButtons = document.querySelectorAll("button.add-to-cart-button");
+    for (const button of addToCartButtons) {
+        button.addEventListener("click", onAddToCartButtonClick);
     }
-    cellTotalPrice.innerHTML = cellOneItemPrice.innerHTML * cellCount.innerHTML;
 }
 
-let cartIconWrap = document.querySelector("span.cartIconWrap");
-cartIconWrap.addEventListener("click", function(event) {
-    console.log("click");
-});
+showOrHideCartCounter();
+addEventClickListinerForAddToCartButtons();
